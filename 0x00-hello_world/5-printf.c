93ca8be99405b71a76513de1ce3a445b6fe135c2
Adebayo Samuel <eladebayoor@gmail.com>
2022-03-10 05:09:06 -0800

    feat: 5-printf.c file added

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+/**
+ * main - prints a phrase
+ *
+ * Return: 0 at the end of the program
+ *
+ *
+ */
+int main(void)
+{
+
+	printf("with proper grammer, but the outcome is a piece of art,\n");
+	return (0);
+
+}
