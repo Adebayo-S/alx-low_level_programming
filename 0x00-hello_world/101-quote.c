d0d97b7e6abefc5ab3f932e4a3e6720e708dad12
Adebayo Samuel <eladebayoor@gmail.com>
2022-03-10 06:11:17 -0800

    fix: added necessary bracket

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -11,7 +11,7 @@ int main(void)
 	char phrase[] = "and that piece of art is useful\" \
 			 - Dora Korpar, 2015-10-19\n";
 
-	write(2, phrase, sizeof(phrase);
+	write(2, phrase, sizeof(phrase));
 	return (1);
 
 }
