5bd09dfae8108c9fe1574537044a64c8539c1039
Adebayo Samuel <eladebayoor@gmail.com>
2022-03-10 04:48:29 -0800

    feat: 4-puts.c file added

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+/*
+ * main - prints a phrase
+ *
+ * return: 0 at the end of the program
+ *
+ *
+ */
+int main(void)
+{
+
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+
+}
