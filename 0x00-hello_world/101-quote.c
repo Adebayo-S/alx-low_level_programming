bd63ef2b3581c162300ea6f3a1cca37c7027b813
Adebayo Samuel <eladebayoor@gmail.com>
2022-03-10 06:40:06 -0800

    changed variable to accomodate 80 char rule

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,9 +8,9 @@
 int main(void)
 {
 
-char phrase[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
-write(2, phrase, 59);
+write(2, str, 59);
 return (1);
 
 }
