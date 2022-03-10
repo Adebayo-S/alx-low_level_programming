3dd98c008333948a50b196ebe9e7af6e924109a4
Adebayo Samuel <eladebayoor@gmail.com>
2022-03-10 06:33:32 -0800

    fix: unify variable use

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,7 +8,7 @@
 int main(void)
 {
 
-char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+char phrase[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
 write(2, phrase, sizeof(phrase));
 return (1);
