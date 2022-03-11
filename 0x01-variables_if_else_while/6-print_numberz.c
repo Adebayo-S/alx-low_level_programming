4474d7f116863eafe509becfd8a686cc36b7dd82
Adebayo Samuel <eladebayoor@gmail.com>
2022-03-11 09:36:17 -0800

    feat: file 6 added

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - print single digit numbers starting from 0
+ *
+ * Return: returns zero ar the end
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
