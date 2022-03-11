b9c449e539f4d661f6acd942278665bad4ab319c
Adebayo Samuel <eladebayoor@gmail.com>
2022-03-11 08:01:47 -0800

    fix: changed n to nLastDigit for comparison

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -19,11 +19,11 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
 	nLastDigit = n % 10;
-	if (n > 5)
+	if (nLastDigit > 5)
 	{
 		printf("Last digit of %d is %d and is greater than 5\n", n, nLastDigit);
 	}
-	else if (n == 0)
+	else if (nLastDigit == 0)
 	{
 		printf("Last digit of %d is %d and is 0\n", n, nLastDigit);
 	}
