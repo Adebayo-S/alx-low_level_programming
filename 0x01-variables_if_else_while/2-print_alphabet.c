65234be74977256f243100b61deb746edeaf2b86
Adebayo Samuel <eladebayoor@gmail.com>
2022-03-11 08:28:41 -0800

    feat: file 2 added

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+* main - Start
+*
+* Return - 0 at the end
+*/
+int main(void)
+{
+	char ch;
+
+	for (ch = "a"; ch <= "z"; ch++)
+	{
+		putchar(ch);
+	}
+	putchar("\n");
+	return (0);
+}
