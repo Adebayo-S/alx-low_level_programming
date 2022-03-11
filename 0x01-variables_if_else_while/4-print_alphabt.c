7deb866eb50f7891ca6d875411a5b09235a5e93a
Adebayo Samuel <eladebayoor@gmail.com>
2022-03-11 09:11:00 -0800

    feat: added file 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - print alphabet in lowercase, and then uppercase
+ * except q and e
+ *
+ * Return: returns zero ar the end
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		if (ch != 'q' || cha != 'e')
+		{
+			putchar(ch);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
