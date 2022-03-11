452ddf01d22ea49925f4bf6225213c188f4bfbb4
Adebayo Samuel <eladebayoor@gmail.com>
2022-03-11 08:56:16 -0800

    feat: file 3 added

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - ilphabet in lowercase, and then uppercase
+ *
+ *
+ * Return: returns zero ar the end
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	for (ch = 'A'; ch <= 'Z'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
