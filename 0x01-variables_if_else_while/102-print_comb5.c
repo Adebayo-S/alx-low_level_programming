d3e6277db8cd352fc8ab9cd5b9cc94a8b203be00
Adebayo Samuel <eladebayoor@gmail.com>
2022-03-11 19:13:38 -0800

    feat: file 12 added

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+/**
+ * main - print possible combo of two 2-digit
+ * numbers
+ *
+ * Return: returns zero ar the end
+ */
+int main(void)
+{
+	int i, j, k;
+
+	for (i = 0; i < 100; i++)
+	{
+		for (j = 0; j < 100; j++)
+		{
+			if (j > i)
+			{
+				putchar((i / 10) + '0');
+				putchar((i % 10) + '0');
+				putchar(' ');
+				putchar((j / 10) + '0');
+				putchar((j % 10) + '0');
+				if (i != 98)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
