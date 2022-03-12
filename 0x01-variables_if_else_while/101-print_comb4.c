2823d08278fc74d99994f615956523bd2fe69fe3
Adebayo Samuel <eladebayoor@gmail.com>
2022-03-11 18:42:55 -0800

    feat: file 11 added

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+/**
+ * main - print possible combo of 3
+ *
+ * Return: returns zero ar the end
+ */
+int main(void)
+{
+	int i, j, k;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 1; j <= 9; j++)
+		{
+			for (k = 2; k <= 9; k++)
+			{
+			if (k > j && j > i)
+			{
+				putchar(i + '0');
+				putchar(j + '0');
+				putchar(k + '0');
+					if (i != 8)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
