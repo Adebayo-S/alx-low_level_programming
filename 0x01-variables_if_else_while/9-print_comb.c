36a26754b1c47775ae470d4b77b39ff9e05ce354
Adebayo Samuel <eladebayoor@gmail.com>
2022-03-11 17:29:51 -0800

    style: code formatting

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -11,11 +11,11 @@ int main(void)
 	for (i = 0; i < 10; i++)
 	{
 		putchar(i + '0');
-	}
-	if ( i < 9)
-	{
+		if (i < 9)
+		{
 		putchar(',');
-		putchar(' ');	
+		putchar(' ');
+		}
 	}
 	putchar('\n');
 	return (0);
