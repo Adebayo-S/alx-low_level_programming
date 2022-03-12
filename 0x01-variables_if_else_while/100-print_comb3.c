96620776f71a66f775b64e4d816337c0cdd37405
Adebayo Samuel <eladebayoor@gmail.com>
2022-03-11 18:09:22 -0800

    fix: fixed code

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - print single digit numbers starting from 0
+ *
+ * Return: returns zero ar the end
+ */
+int main(void)
+{
+	int i, j;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 1; j <= 9; j++)
+		{
+			if (j > i)
+			{
+				putchar(i + '0');
+				putchar(j + '0');
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
