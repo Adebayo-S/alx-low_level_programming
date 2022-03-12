6ae59455e6c2ea323cef5e482ef7361f1a7101d6
Adebayo Samuel <eladebayoor@gmail.com>
2022-03-11 17:24:51 -0800

    fix: created new file 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -11,12 +11,12 @@ int main(void)
 	for (i = 0; i < 10; i++)
 	{
 		putchar(i + '0');
-        if (i < 9)
-        {
-                putchar(',');
-                putchar(' ');        
+	}
+	if ( i < 9)
+	{
+		putchar(',');
+		putchar(' ');	
 	}
 	putchar('\n');
 	return (0);
-	}
 }
