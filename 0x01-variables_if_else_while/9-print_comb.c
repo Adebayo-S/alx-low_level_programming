3ae0ba639e07e14d8bfbde6f6961d2fc0f10162e
Adebayo Samuel <62617476+Adebayo-S@users.noreply.github.com>
2022-03-12 01:20:24 +0100

    docs: updated README.md

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -12,10 +12,10 @@ int main(void)
 	for (i = 0; i < 10; i++)
 	{
 		putchar(i + '0');
-	}
-	for (ch = 'a'; ch <= 'f'; ch++)
-	{
-		putchar(ch);
+        if (i < 9)
+        {
+                putchar(',');
+                putchar(' ');        
 	}
 	putchar('\n');
 	return (0);
