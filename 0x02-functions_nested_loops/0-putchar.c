6c10891a0006381b049d6cc02f508d7bcf767880
Adebayo Samuel <eladebayoor@gmail.com>
2022-03-15 16:27:19 -0700

    feat: file 0 added

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+include "main.h"
+
+/**
+* main - Print the word "_putchar"
+*
+* Return: Always 0
+*/
+
+int main(void)
+{
+	int i;
+	char p[] = "_putchar";
+
+	for (i = 0; i < 9; i++)
+	{
+		_putchar(p[i]);
+	}
+	_putchar('\n');
+
+	return (0);
+}
