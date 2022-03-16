78ab671684eb4e69c45ed0643bf3be851de373d4
Adebayo Samuel <eladebayoor@gmail.com>
2022-03-16 07:18:16 -0700

    fix: matched main.c from example code

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,10 +1,8 @@
 #include "main.h"
 
 /**
-* main - tests the function that
-* prints if the integer is positive or negative
-*
-* Return: (0)
+* main - tests function that prints if integer is positive or negative
+* Return: 0
 */
 
 int main(void)
