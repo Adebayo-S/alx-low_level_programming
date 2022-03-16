ca8bec1e30eb4e1a22cb4dc74f5fbdc1212a6f5e
Adebayo Samuel <eladebayoor@gmail.com>
2022-03-16 08:10:44 -0700

    fix: nested the for loop in the if statement

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -3,7 +3,7 @@
 /**
  * print_times_table - prints the n times table, starting with 0
  *
- *@n: number times table (0 < n <= 15)
+ * @n: number times table (0 < n <= 15)
  *
  * Returns- void
  */
@@ -13,39 +13,39 @@ void print_times_table(int n)
 
 	int i, j, k;
 
-	if (n > 15 || n < 0)
-		break;
-
-	for (i = 0; i <= n; i++)
+	if (n >= 0 && n <= 15)
 	{
-		for (j = 0; j <= n; j++)
+		for (i = 0; i <= n; i++)
 		{
-			k = j * i;
-			if (k < 10)
+			for (j = 0; j <= n; j++)
 			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar(' ');
-				_putchar(' ');
-				_putchar(k + '0');
+				k = j * i;
+				if (k < 10)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(k + '0');
+				}
+				else if (k < 100)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar((k / 10) + '0');
+					_putchar((k % 10) + '0');
+				}
+				else
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar((k / 100) + '0');
+					_putchar(((k % 100) / 10) + '0');
+					_putchar(((k % 100) % 10) + '0');
+				}
 			}
-			else if (k < 100)
-			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar(' ');
-				_putchar((k / 10) + '0');
-				_putchar((k % 10) + '0');
-			}
-			else
-			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar((k / 100) + '0');
-				_putchar(((k % 100) / 10) + '0');
-				_putchar(((k % 100) % 10) + '0');
-			}
+				_putchar('\n');
 		}
-			_putchar('\n');
 	}
 }
