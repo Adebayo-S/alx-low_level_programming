2e537bc763653a99fb72435ca178050e488d1f62
Adebayo Samuel <eladebayoor@gmail.com>
2022-03-16 08:16:01 -0700

    fix: added the conditional if second counter = 0

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -20,7 +20,11 @@ void print_times_table(int n)
 			for (j = 0; j <= n; j++)
 			{
 				k = j * i;
-				if (k < 10)
+				if (j == 0)
+				{
+					_putchar(k + '0');
+				}
+				else if (k < 10)
 				{
 					_putchar(',');
 					_putchar(' ');
