c10817497be2d03804e57bc84214883a92a7808b
Adebayo Samuel <eladebayoor@gmail.com>
2022-03-16 12:27:58 -0700

    fix: changed typeof fibonacci num to uint long to correct overfloat

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -7,14 +7,15 @@
 
 int main(void)
 {
-	int i, fib, prevFib, prev2Fib;
+	int i;
+	unsigned long fib, prevFib, prev2Fib;
 
 	prevFib = 1, prev2Fib = 0;
 
 	for (i = 0; i < 50; i++)
 	{
 		fib = prevFib + prev2Fib;
-		printf("%d", fib);
+		printf("%lu", fib);
 
 		prev2Fib = prevFib;
 		prevFib = fib;
