14c0e6ed5e3d09216b0f76e5895102626f4c566d
Adebayo Samuel <eladebayoor@gmail.com>
2022-03-16 08:37:08 -0700

    style: reduced number of lines in function

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * print_times_table - prints the n times table, starting with 0
  *
@@ -7,10 +6,8 @@
  *
  * Returns- void
  */
-
 void print_times_table(int n)
 {
-
 	int i, j, k;
 
 	if (n >= 0 && n <= 15)
@@ -21,9 +18,7 @@ void print_times_table(int n)
 			{
 				k = j * i;
 				if (j == 0)
-				{
 					_putchar(k + '0');
-				}
 				else if (k < 10)
 				{
 					_putchar(',');
