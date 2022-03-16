0f0035ff93d851025d956234447fa9a1e03205a2
Adebayo Samuel <eladebayoor@gmail.com>
2022-03-16 13:29:32 -0700

    style: formatted to at most 40 lines in a function

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -8,7 +8,6 @@
 
 int main(void)
 {
-
 	int count;
 	unsigned long fib1 = 0, fib2 = 1, sum;
 	unsigned long fib1_half1, fib1_half2, fib2_half1, fib2_half2;
@@ -18,11 +17,9 @@ int main(void)
 	{
 		sum = fib1 + fib2;
 		printf("%lu, ", sum);
-
 		fib1 = fib2;
 		fib2 = sum;
 	}
-
 	fib1_half1 = fib1 / 10000000000;
 	fib2_half1 = fib2 / 10000000000;
 	fib1_half2 = fib1 % 10000000000;
@@ -37,7 +34,6 @@ int main(void)
 			half1 += 1;
 			half2 %= 10000000000;
 		}
-
 		printf("%lu%lu", half1, half2);
 		if (count != 98)
 			printf(", ");
