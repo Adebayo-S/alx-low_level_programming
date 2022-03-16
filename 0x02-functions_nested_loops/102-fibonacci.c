2c56e02a237bffd031bc0c5ec98cd643806c45b5
Adebayo Samuel <eladebayoor@gmail.com>
2022-03-16 12:24:12 -0700

    feat: file 102 added

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - Prints the first 50 Fibonacci numbers, starting with 1 and 2,
+ *
+ * Return: Always (0).
+ */
+
+int main(void)
+{
+	int i, fib, prevFib, prev2Fib;
+
+	prevFib = 1, prev2Fib = 0;
+
+	for (i = 0; i < 50; i++)
+	{
+		fib = prevFib + prev2Fib;
+		printf("%d", fib);
+
+		prev2Fib = prevFib;
+		prevFib = fib;
+		if (i == 49)
+			printf("\n");
+		else
+			printf(", ");
+	}
+
+	return (0);
+}
