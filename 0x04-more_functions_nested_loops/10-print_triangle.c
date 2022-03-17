4f5bb8a806ee5117df9780f43fdaeab8ff3f1fa1
Adebayo Samuel <eladebayoor@gmail.com>
2022-03-17 16:50:10 -0700

    feat: file 10 added

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+* print_triangle - prints a triangle
+* @size: takes in a integer for size of triangle
+*/
+
+void print_triangle(int size)
+{
+	int row, column, k;
+
+	if (size <= 0)
+		_putchar('\n');
+	for (column = 0; column < size; column++)
+	{
+		for (row = 0; row < size; row++)
+		{
+			if (row >= size - column)
+				_putchar('#');
+			else
+				_putchar(' ');
+		}
+		_putchar('\n');
+	}
+}
