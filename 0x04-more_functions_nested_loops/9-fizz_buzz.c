606ebaa78ee79132e21e993b1ada2f7061783612
Adebayo Samuel <eladebayoor@gmail.com>
2022-03-17 16:13:24 -0700

    fix: moved (i == 100) condition to (i % 5) block

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -14,14 +14,14 @@ int main(void)
 	{
 		if (i % 15 == 0)
 		{
-			if (i == 100)
-				printf("FizzBuzz");
-			else
-				printf("FizzBuzz ");
+			printf("FizzBuzz ");
 		}
 		else if (i % 5 == 0)
 		{
-			printf("Buzz ");
+			if (i == 100)
+				printf("Buzz");
+			else
+				printf("Buzz ");
 		}
 		else if (i % 3 == 0)
 		{
