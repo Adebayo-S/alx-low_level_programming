5fc1810783a4614f9e98ea5ba650c5ad90781f4b
Adebayo Samuel <eladebayoor@gmail.com>
2022-03-17 14:13:15 -0700

    fix: modified if block

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -13,12 +13,8 @@ void more_numbers(void)
 		for (j = 0; j <= 14; j++)
 		{
 			if (j > 9)
-			{
 				_putchar((j / 10) + '0');
-				_putchar((j % 10) + '0');
-			}
-			else
-				_putchar(j + '0');
+			_putchar((j % 10) + '0');
 		}
 
 		_putchar('\n');
