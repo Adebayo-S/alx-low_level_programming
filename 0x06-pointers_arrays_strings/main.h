38524dae029d8e2b54c480c5c016181a62bf7afa
Adebayo Samuel <eladebayoor@gmail.com>
2022-03-23 16:48:27 -0700

    fix: file 101 prototype added to header

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -13,5 +13,6 @@ char *_strstr(char *haystack, char *needle);
 void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
 void set_string(char **s, char *to);
+void print_number(int n);
 
 #endif
