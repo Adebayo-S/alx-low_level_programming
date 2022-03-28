a4965e34e7d9b54970765f90b785bc2dbf750f19
Adebayo Samuel <eladebayoor@gmail.com>
2022-03-28 15:52:56 -0700

    style: deleted whitespaces

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -6,12 +6,12 @@
  *
  * Return: The number of bytes in s which
  *         consist only of bytes from accept.
- * 
+ *
  * PSEUDOCODE
  * Loop through s and compare s[i] to values in accept
  *	increase bytes counter when values in accept can be found
  *	*in s
- *	end loop and return bytes when accept is exhaused	
+ * end loop and return bytes when accept is exhaused
  */
 unsigned int _strspn(char *s, char *accept)
 {
