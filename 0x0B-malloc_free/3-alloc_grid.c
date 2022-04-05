e51839f47c3ff36f8a5069a77b18ea1aa7637285
Adebayo Samuel <eladebayoor@gmail.com>
2022-04-05 16:07:45 -0700

    style: meet betty linter requirements

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -13,35 +13,35 @@
  */
 int **alloc_grid(int width, int height)
 {
-  int i, j, **grid_array;
+	int i, j, **grid_array;
 
-  if (width < 1 || height < 1)
-    return (NULL);
+	if (width < 1 || height < 1)
+		return (NULL);
 
-  grid_array = malloc(height * sizeof(int *));
+	grid_array = malloc(height * sizeof(int *));
 
-  if (grid_array == NULL)
-  {
-    free(grid_array);
-    return (NULL);
-  }
+	if (grid_array == NULL)
+	{
+		free(grid_array);
+		return (NULL);
+	}
 
-  for (i = 0; i < height; i++)
-  {
-    grid_array[i] = malloc(sizeof(int) * width);
-    if (grid_array[i] == NULL)
-    {
-      while (--i >= 0)
-        free(grid_array[i]);
-      free(grid_array);
-      return (NULL);
-    }
-  }
+	for (i = 0; i < height; i++)
+	{
+		grid_array[i] = malloc(sizeof(int) * width);
+		if (grid_array[i] == NULL)
+		{
+			while (--i >= 0)
+				free(grid_array[i]);
+			free(grid_array);
+			return (NULL);
+		}
+	}
 
-  for (i = 0; i < height; i++)
-  {
-    for (j = 0; j < width; j++)
-      grid_array[i][j] = 0;
-  }
-  return (grid_array);
+	for (i = 0; i < height; i++)
+	{
+		for (j = 0; j < width; j++)
+			grid_array[i][j] = 0;
+	}
+	return (grid_array);
 }
