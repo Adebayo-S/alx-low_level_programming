b02ce163f5a44b255a6b5cb37b45926142a737e4
Adebayo Samuel <eladebayoor@gmail.com>
2022-04-07 09:39:10 -0700

    fix: renamed file

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -14,26 +14,26 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-unsigned int i;
-unsigned int j;
-unsigned int s1_len = strlen(s1);
-char *concat;
+	unsigned int i;
+	unsigned int j;
+	unsigned int s1_len = strlen(s1);
+	char *concat;
 
-int concat_len = s1_len + n;
+	int concat_len = s1_len + n;
 
-concat = malloc(sizeof(char) * (concat_len + 1));
-if (concat == NULL)
-    return (NULL);
+	concat = malloc(sizeof(char) * (concat_len + 1));
+	if (concat == NULL)
+		return (NULL);
 
-for (i = 0; i < s1_len; i++)
-{
-concat[i] = s1[i];
-}
+	for (i = 0; i < s1_len; i++)
+	{
+		concat[i] = s1[i];
+	}
 
-for (j = 0; j < n; j++, i++)
-{
-concat[i] = s2[j];
-}
+	for (j = 0; j < n; j++, i++)
+	{
+		concat[i] = s2[j];
+	}
 
-return (concat);
+	return (concat);
 }
