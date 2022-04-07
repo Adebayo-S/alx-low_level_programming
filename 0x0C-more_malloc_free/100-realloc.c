fbaf994bf93f6d3da62c0ae2f5ed1cfad0db005e
Adebayo Samuel <eladebayoor@gmail.com>
2022-04-08 00:59:41 +0100

    feat: file 100 added

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,70 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _realloc - reallocates a memory block.
+ * @ptr: pointer to the memory previously allocated.
+ * @old_size: size, in bytes, of the allocated space of ptr.
+ * @new_size: new size, in bytes, of the new memory block.
+ *
+ * Return: ptr.
+ * if new_size == old_size, returns ptr without changes.
+ * if malloc fails, returns NULL.
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	int i, j, numb, sizeToAdd, sizeToRemove, addedSpace;
+	void **modPtr;
+
+	modPtr = ptr;
+
+	addedSpace = old_size % sizeof(unsigned int);
+
+	if (addedSpace == 0)
+		numb = old_size / sizeof(unsigned int);
+	else
+		numb = (old_size / sizeof(unsigned int)) + addedSpace;
+
+	if (ptr == NULL)
+	{
+		free(ptr);
+		ptr = malloc(new_size);
+		if (ptr == NULL)
+			return (NULL);
+		return (ptr);
+	}
+
+	if (new_size == 0 && ptr != NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+
+	if (old_size == new_size)
+		return (ptr);
+	else if (new_size > old_size)
+	{
+		sizeToAdd = new_size - old_size;
+
+		for (i = 0, j = numb; i < sizeToAdd; i++, j++)
+		{
+			*(modPtr + j) = malloc(1);
+			if (*(modPtr + j) == NULL)
+				return (NULL);
+		}
+
+		return (ptr);
+	}
+	else
+	{
+		sizeToRemove = old_size - new_size;
+
+		for (i = 0, j = numb - 1; i < sizeToRemove; i++, j--)
+		{
+			*(modPtr + j) = malloc(0);
+			if (*(modPtr + j) == NULL)
+				return (NULL);
+		}
+
+		return (ptr);
+	}
+}
