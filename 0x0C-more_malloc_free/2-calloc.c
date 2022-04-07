ac8715976ae6da3e84c916a66fc14ede5af2e8a0
Adebayo Samuel <eladebayoor@gmail.com>
2022-04-07 21:47:42 +0100

    fix: changed end condition in for loop to reflect full size

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -23,7 +23,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (array == NULL)
 		return (NULL);
 
-	for (i = 0; i < nmemb; i++)
+	for (i = 0; i < nmemb * size; i++)
 		array[i] = 0;
 
 	return ((void*)array);
