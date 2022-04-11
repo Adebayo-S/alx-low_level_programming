d1246c92e21e0d9a4b0e9fdaef810a7987a46778
Adebayo Samuel <eladebayoor@gmail.com>
2022-04-11 22:36:38 +0100

    feat dog.h added

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+
+/**
+ * struct dog - A new type describing a dog.
+ * @name: The name of the dog.
+ * @age: The age of the dog.
+ * @owner: The owner of the dog.
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
