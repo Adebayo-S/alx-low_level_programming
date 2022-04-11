da1f8d19d2b65def157b5f8252f220ab56d9178a
Adebayo Samuel <eladebayoor@gmail.com>
2022-04-11 23:28:47 +0100

    fix: added conditions for NULL

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,28 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * print_dog - Prints a struct dog.
+ * @d: The struct dog to be printed.
+ */
+void print_dog(struct dog *d)
+{
+	if (d)
+	{
+		if (d->name)
+			printf("Name: %s\n", d->name);
+		else
+			printf("Name: (nil)\n");
+
+		if (d->age)
+			printf("Age: %.06f\n", d->age);
+		else
+			printf("Age: (nil)\n");
+
+		if (d->owner)
+			printf("Owner: %s\n", d->owner);
+		else
+			printf("Owner: (nil)\n");
+	}
+}
