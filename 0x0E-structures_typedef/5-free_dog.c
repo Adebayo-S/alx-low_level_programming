2608947a67f5a3f9979073869d34d585001ac241
Adebayo Samuel <eladebayoor@gmail.com>
2022-04-12 01:22:00 +0100

    feat: file 5 added

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,18 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * free_dog - Frees dogs.
+ * @d: The dog to be freed.
+ */
+void free_dog(dog_t *d)
+{
+	if (d)
+	{
+		if (d->name)
+			free(d->name);
+		if (d->owner)
+			free(d->name);
+		free(d);
+	}
+}
