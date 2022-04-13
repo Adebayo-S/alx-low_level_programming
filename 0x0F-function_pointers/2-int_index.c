6d6300a819784c0d5e390858b624f6b0ecff43b9
Adebayo Samuel <eladebayoor@gmail.com>
2022-04-13 23:02:23 +0100

    fix: changed return type on line 21 to (-1)

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,14 +1,23 @@
-#include <stdio.h>
 #include "function_pointers.h"
+/**
+ * int_index - Searches for an integer in an array of integers.
+ * @array: The array of integers.
+ * @size: The size of the array.
+ * @cmp: A pointer to the function to be used to compare values.
+ *
+ * Return: If no element matches or size <= 0 - -1.
+ *         Otherwise - The index of the first element for which
+ *                     the cmp function does not return 0.
+ */
 
 int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	if (!array || size == NULL || !cmp)
-		return;
-
 	if (size <= 0)
+		return (-1);
+
+	if (!array || !size || !cmp)
 		return (-1);
 
 	for (i = 0; i < size; i++)
