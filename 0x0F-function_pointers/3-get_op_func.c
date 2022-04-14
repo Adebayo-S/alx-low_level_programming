569a2b9a378ff4f356423eadd1657eee63d992b9
Adebayo Samuel <eladebayoor@gmail.com>
2022-04-14 04:28:11 +0100

    fix: line 24, condition for while loop

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -21,12 +21,12 @@ int (*get_op_func(char *s))(int, int)
 
 	int i = 0;
 
-	while (i < 6)
+	while (ops[i].op)
 	{
 		if (*s == *(ops[i].op))
 			return (ops[i].f);
 		i++;
 	}
 
-	return (ops[i].f);
+	return (NULL);
 }
