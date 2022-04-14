ed3f97068dead813605bc035bd3cf7a73df79a3a
Adebayo Samuel <eladebayoor@gmail.com>
2022-04-14 03:07:36 +0100

    fix: corrected 3-main.c:line 32

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -28,7 +28,7 @@ int main(int argc, char *argv[])
 	a = atoi(argv[1]);
 	b = atoi(argv[3]);
 
-	if ((argv[0][0] == '/' || argv[0][0] == '%') && (b == 0))
+	if ((argv[2][0] == '/' || argv[2][0] == '%') && (b == 0))
 	{
 		printf("Error\n");
 		return (100);
