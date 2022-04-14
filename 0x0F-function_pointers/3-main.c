0fb5221a6b34f95bf61d2eff291214f7c9d3f1fc
Adebayo Samuel <eladebayoor@gmail.com>
2022-04-14 02:58:09 +0100

    fix: prioritized error handling

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -19,22 +19,22 @@ int main(int argc, char *argv[])
 		return (98);
 	}
 
+	if (get_op_func(argv[2]) || argv[2][1])
+	{
+		printf("Error\n");
+		return (99);
+	}
+
 	a = atoi(argv[1]);
 	b = atoi(argv[3]);
 
 	if ((argv[0][0] == '/' || argv[0][0] == '%') && (b == 0))
 	{
 		printf("Error\n");
-		exit(100);
+		return (100);
 	}
 
 	res = (*get_op_func(argv[2]))(a, b);
-
-	if (!res || argv[2][1])
-	{
-		printf("Error\n");
-		return (99);
-	}
 
 	printf("%d\n", res);
 	return (0);
