37b7de593973274c3d4bbe29ae8f2f6116f4221b
Adebayo Samuel <eladebayoor@gmail.com>
2022-04-14 01:55:12 +0100

    feat: file 3 added

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,40 @@
+#include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - check the code for Holberton School students.
+ * @argc: argument count.
+ * @argv: argument vector.
+ *
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+	int a, b, res;
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		return (98);
+	}
+
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+
+	if ((argv[2][0] == '/' || argv[2][0] == '%') && b == 0)
+	{
+		printf("Error\n");
+		return (100);
+	}
+
+	res = (*get_op_func(argv[2]))(a, b);
+	if (!res)
+	{
+		printf("Error\n");
+		return (99);
+	}
+
+	printf("%d\n", res);
+	return (0);
+}
