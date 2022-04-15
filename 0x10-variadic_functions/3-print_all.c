413222072c319b9067cbc9f8ea74348606175e62
Adebayo Samuel <eladebayoor@gmail.com>
2022-04-15 03:10:29 +0100

    fix: restructure code

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -11,7 +11,7 @@
 void print_all(const char * const format, ...)
 {
 	va_list list;
-	unsigned int i = 0, n = 0;
+	unsigned int i, n;
 	char *str;
 
 	va_start(list, format);
@@ -22,22 +22,26 @@ void print_all(const char * const format, ...)
 		{
 		case 'c':
 			printf("%c", va_arg(list, int)), n++;
+			break;
 		case 'i':
 			printf("%d", va_arg(list, int)), n++;
+			break;
 		case 'f':
 			printf("%f", va_arg(list, double)), n++;
+			break;
 		case 's':
 			str = va_arg(list, char *), n++;
 			if (!str)
 			{
 				printf("(nil)");
+				break;
 			}
 			printf("%s", str);
+			break;
 		}
 		if (i < n - 1)
 			{
 				printf(", ");
-				break;
 			}
 		i++;
 	}
