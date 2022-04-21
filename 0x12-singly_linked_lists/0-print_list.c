eec1d58fb3d5eeb737942b5b32d6ff0215e1d2b6
Adebayo Samuel <eladebayoor@gmail.com>
2022-04-21 22:58:32 +0100

    [fix]: removed whitespace in printf on line:18

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -15,7 +15,7 @@ size_t print_list(const list_t *h)
 		if (h->str == NULL)
 			printf("[0] (nil)\n");
 		else
-			printf("[%d] %s \n", h->len, h->str);
+			printf("[%d] %s\n", h->len, h->str);
 		h = h->next;
 		i++;
 	}
