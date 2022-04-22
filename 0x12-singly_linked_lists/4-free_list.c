45caee4eba6f22a8c7d24a6b9573b677871fe61e
Adebayo Samuel <eladebayoor@gmail.com>
2022-04-22 01:47:26 +0100

    [feat]: file 4 added

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * free_list - Frees list.
+ * @head: The list.
+ */
+void free_list(list_t *head)
+{
+	list_t *temp = NULL;
+
+	while(head)
+	{
+		temp = head;
+		free(head);
+		free(head->str);
+		temp = head->next;
+	}
+}
