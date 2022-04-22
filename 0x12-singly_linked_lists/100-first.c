fdc7c88428d17b1d61755d4fde3780d42b4a4327
Adebayo Samuel <eladebayoor@gmail.com>
2022-04-22 02:54:41 +0100

    [style]: betty style requirements

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -4,7 +4,8 @@
  * constructorRun - runs
  *  before main function is executed.
  */
-void constructorRun (void)
+void constructorRun(void)
 {
-printf ("You're beat! and yet, you must allow,\nI bore my house upon my back!\n\n");
+	printf ("You're beat! and yet, you must allow,\n"
+		"I bore my house upon my back!\n");
 }
