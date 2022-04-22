8da2573bc3835b412d3a39e1bb1d40fd54450612
Adebayo Samuel <eladebayoor@gmail.com>
2022-04-22 02:40:10 +0100

    [fix]: line33 - 36 restructure

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -9,7 +9,7 @@
  */
 list_t *add_node_end(list_t **head, const char *str)
 {
-	list_t *new = NULL;
+	list_t *new, *last;
 	size_t i = 0;
 
 	new = malloc(sizeof(list_t));
@@ -30,10 +30,11 @@ list_t *add_node_end(list_t **head, const char *str)
 		*head = new;
 	else
 	{
-		while ((*head)->next != NULL)
-			*head = (*head)->next;
-		(*head)->next = new;
+		last = *head;
+		while (last->next != NULL)
+			last = last->next;
+		last->next = new;
 	}
 
-	return (*head);
+	return (new);
 }
