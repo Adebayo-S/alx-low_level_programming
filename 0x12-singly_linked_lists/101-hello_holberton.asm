49ae4310042cd7b489d79caef435c377cf0a897d
Adebayo Samuel <eladebayoor@gmail.com>
2022-04-23 18:51:46 -0700

    [fix]: code refactor

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,15 +1,17 @@
-section .data
-    message db "Hello, Holberton", 10
+global main
 
-section .text
+	section .text
 
-global _start
-_start:
-    mov rax, 1
-    mov rdi, 1
-    mov rsi, message
-    mov rdx, 17
-    syscall
+main:
+	mov rax, 1
+	mov rdi, 1
+	mov rsi, msg
+	mov rdx, msglen
+	syscall
 	mov rax, 60
 	mov rdi, 0
 	syscall
+
+	section .rodata
+msg:		 db "Hello, Holberton", 10
+msglen:		 equ $ - msg
