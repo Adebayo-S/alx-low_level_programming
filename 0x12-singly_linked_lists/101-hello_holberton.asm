939effef85f7bea0dca591679a4a5d71bc0a3c15
Adebayo Samuel <eladebayoor@gmail.com>
2022-04-23 18:08:53 -0700

    [feat]: file 101 added

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,15 @@
+section .data
+    message db "Hello, Holberton", 10
+
+section .text
+
+global _start
+_start:
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, message
+    mov rdx, 17
+    syscall
+	mov rax, 60
+	mov rdi, 0
+	syscall
