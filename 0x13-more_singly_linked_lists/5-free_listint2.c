419f9ec1d3dd7ed97ccf2e0674fe96d084dee79d
Adebayo Samuel <eladebayoor@gmail.com>
2022-04-26 23:03:25 +0100

    [fix]: code change

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -4,19 +4,18 @@
  * free_listint2 - Frees a listint_t list
  *        and sets the head to NULL.
  * @head: A pointer to the address of the
- *        head of the listint_t list.
- *
+ *        head of the listint_t list. *
  */
 void free_listint2(listint_t **head)
 {
 	listint_t *temp = *head;
 
 
-	while (head)
+	while (*head)
 	{
 		temp = temp->next;
 		free(head);
-		head = temp;
+		*head = temp;
 	}
 
 	*head = NULL;
