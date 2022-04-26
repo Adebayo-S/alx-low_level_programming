c2673f215b75a8a6cbd50d324ab02dda72813a67
Adebayo Samuel <eladebayoor@gmail.com>
2022-04-26 22:45:59 +0100

    [style]: added comments

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,5 +1,12 @@
 #include "lists.h"
 
+/**
+ * free_listint2 - Frees a listint_t list
+ *        and sets the head to NULL.
+ * @head: A pointer to the address of the
+ *        head of the listint_t list.
+ *
+ */
 void free_listint2(listint_t **head)
 {
 	listint_t *temp = *head;
