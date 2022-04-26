f17ae2bed6d6aa4e7b098083dc048df65aa66320
Adebayo Samuel <eladebayoor@gmail.com>
2022-04-26 14:09:33 +0100

    [style]: line:12 put blank space after declaration

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -9,6 +9,7 @@
 size_t print_listint(const listint_t *h)
 {
 	size_t i = 0;
+
 	while (h)
 	{
 		printf("%d\n", h->n);
--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -10,7 +10,6 @@
  * @next: points to the next node
  *
  * Description: singly linked list node structure
- * for Holberton project
  */
 typedef struct listint_s
 {
