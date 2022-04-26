fe68e1301cf7cabcc961b377c2d7642d1588c14d
Adebayo Samuel <eladebayoor@gmail.com>
2022-04-27 00:31:32 +0100

    [fix]: corrected line 23 to free(*head)

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -10,14 +10,19 @@
  */
 int pop_listint(listint_t **head)
 {
-	listint_t *temp = *head;
-	int data = temp->n;
+	listint_t *temp;
+	int data;
 
 	if (!(*head))
 		return (0);
 
+	temp = *head;
+	data = temp->n;
 	temp = temp->next;
-	free(head);
+
+	free(*head);
+
 	*head = temp;
+
 	return (data);
 }
