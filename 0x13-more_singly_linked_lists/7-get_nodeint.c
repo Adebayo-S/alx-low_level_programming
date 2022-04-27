8c152e2035045dbf93ee017e1910f181faa36248
Adebayo Samuel <eladebayoor@gmail.com>
2022-04-27 01:37:23 +0100

    [style]: Betty coding requirements, space after while, line:14

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -11,7 +11,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	unsigned int i = 0;
 
-	while(head != NULL)
+	while (head != NULL)
 	{
 		if (i == index)
 			return (head);
