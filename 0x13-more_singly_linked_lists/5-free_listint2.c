f7589cd7348fd5dc133a65247cae1981bd435d15
Adebayo Samuel <eladebayoor@gmail.com>
2022-04-27 04:59:08 +0100

    [fix]: line:18 corrected

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -8,14 +8,14 @@
  */
 void free_listint2(listint_t **head)
 {
-	listint_t *temp = *head;
+	listint_t *temp;
 
 	if (head == NULL)
 		return;
 
 	while (*head)
 	{
-		temp = temp->next;
+		temp = (*head)->next;
 		free(*head);
 		*head = temp;
 	}
