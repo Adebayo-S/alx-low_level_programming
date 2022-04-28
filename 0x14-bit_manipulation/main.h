41f836b912732683d71e364399406ecf1085c141
Adebayo Samuel <eladebayoor@gmail.com>
2022-04-29 00:07:32 +0100

    [fix]: added the helper function prototypes to main.h

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -2,6 +2,8 @@
 #define MAIN_H
 
 int _putchar(char c);
+unsigned int _strlen(char const *str);
+unsigned int _pow(unsigned int a, unsigned int b);
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
