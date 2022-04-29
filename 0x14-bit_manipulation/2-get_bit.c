3e3e2b9f945eb857f6e2d4d2adfda9698f28b324
Adebayo Samuel <eladebayoor@gmail.com>
2022-04-29 03:48:09 +0100

    [feat]: file 2 added

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,31 +1,17 @@
 #include "main.h"
 #include "stdlib.h"
 
-
+/**
+ * get_bit - returns the value of a bit at an index
+ * @n: number
+ * @index: index of the bit
+ *
+ * Return: value of the bit
+ */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	unsigned long int i, len = 0, bin = 1, temp = n;
-	unsigned long int *binary;
+	if (index > 63)
+		return (-1);
 
-	while (temp > 1)
-	{
-		temp /= 2;
-		bin *= 2;
-		len++;
-	}
-
-	binary = malloc(sizeof(unsigned long int) * len);
-
-	temp = n;
-
-	i = 0;
-	while (bin > 0)
-	{
-		binary[i] = (temp / bin);
-		temp %= bin;
-		bin /= 2;
-		i++;
-	}
-
-	return (binary[len - index]);
+	return ((n >> index) & 1);
 }
--- 0x14-bit_manipulation/2-main.c
+++ 0x14-bit_manipulation/2-main.c
@@ -16,5 +16,9 @@ int main(void)
     printf("%d\n", n);
     n = get_bit(1024, 0);
     printf("%d\n", n);
+    n = get_bit(8, 0);
+    printf("%d\n", n);
+    n = get_bit(5, 1);
+    printf("%d\n", n);
     return (0);
 }
