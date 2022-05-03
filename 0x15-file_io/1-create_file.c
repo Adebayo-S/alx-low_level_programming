269bb95acd8462e62ffd49e2de070a43d9a76e59
Adebayo Samuel <eladebayoor@gmail.com>
2022-05-03 04:16:00 +0100

    [fix]: added !text_content condition

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -15,11 +15,11 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	if (text_content != NULL)
-	{
-		for (len = 0; *(text_content + len); len++)
-			;
-	}
+	if (!text_content)
+		text_content = "";
+
+	for (len = 0; *(text_content + len); len++)
+		;
 
 	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	w = write(fd, text_content, len);
