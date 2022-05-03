0e5c259ad0fba0a932f629ad94f3bad728a5852c
Adebayo Samuel <eladebayoor@gmail.com>
2022-05-03 03:56:23 +0100

    [feat]: file 0 added

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * read_textfile - Reads a text file and prints it to POSIX stdout.
+ * @filename: A pointer to the name of the file.
+ * @letters: The number of letters the
+ *           function should read and print.
+ *
+ * Return: If the function fails or filename is NULL - 0.
+ *         O/w - the actual number of bytes the function can read and print.
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	ssize_t fd, r, w;
+	char *buffer;
+
+	if (!filename)
+		return (0);
+
+	buffer = malloc(letters * sizeof(char));
+	if (!buffer)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+	r = read(fd, buffer, letters);
+	w = write(STDOUT_FILENO, buffer, r);
+
+	if (fd == -1 || r == -1 || w == -1 || w != r)
+	{
+		free(buffer);
+		return (0);
+	}
+
+	free(buffer);
+	close(fd);
+
+	return (w);
+}
