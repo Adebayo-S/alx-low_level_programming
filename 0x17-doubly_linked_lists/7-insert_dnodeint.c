9c939fca7e7bc7a5ebd4272f651996b8421d0dbe
Adebayo Samuel <eladebayoor@gmail.com>
2022-06-10 01:33:25 +0100

    [fix]: if the index is at the end

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -27,6 +27,8 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	{
 		if (idx == i)
 		{
+			if (temp->next == NULL)
+				return (add_dnodeint_end(h, n));
 			temp->prev->next = new;
 			new->prev = temp->prev;
 			new->next = temp;
