0dabdc1dee45e672360c91455ddb95d329c7f39e
Adebayo Samuel <eladebayoor@gmail.com>
2022-06-10 02:13:37 +0100

    [style]: betty requirements

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -14,7 +14,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	unsigned int i = 0;
 
 	if (!(*head))
-		return -1;
+		return (-1);
 
 	while (temp && i != index)
 	{
@@ -23,7 +23,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	}
 
 	if (!temp)
-		return -1;
+		return (-1);
 	else if (temp == *head)
 	{
 		*head = (*head)->next;
