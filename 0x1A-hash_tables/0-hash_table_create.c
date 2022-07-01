57321e9eb468a10e9e3d60086d11bbd7a69aa4eb
Adebayo Samuel <eladebayoor@gmail.com>
2022-07-01 01:31:14 +0100

    [feat]: file 0 added

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,21 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - creates a hash table
+ * @size: size of the array
+ *
+ * Return: pointer to new hash table or NULL on error.
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *hashtable = NULL;
+
+	hashtable = malloc(sizeof(hash_table_t));
+	if (!hashtable || !size)
+		return NULL;
+
+	hashtable->size = size;
+	hashtable->array = malloc(sizeof(hash_node_t*) * size);
+
+	return hashtable;
+}
