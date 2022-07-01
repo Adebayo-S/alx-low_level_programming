e06860aa76e00841461a555b2d45a1d0a6a252ce
Adebayo Samuel <eladebayoor@gmail.com>
2022-07-01 01:34:17 +0100

    [style]: betty style requirements

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -12,10 +12,10 @@ hash_table_t *hash_table_create(unsigned long int size)
 
 	hashtable = malloc(sizeof(hash_table_t));
 	if (!hashtable || !size)
-		return NULL;
+		return (NULL);
 
 	hashtable->size = size;
 	hashtable->array = malloc(sizeof(hash_node_t*) * size);
 
-	return hashtable;
+	return (hashtable);
 }
