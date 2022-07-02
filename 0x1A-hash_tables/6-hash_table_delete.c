88528ca2371e1f357ab4df1aff311445a53a2464
Adebayo Samuel <eladebayoor@gmail.com>
2022-07-02 02:56:45 +0100

    [fix]: removed condition to check if hashtale cells are null

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -12,19 +12,19 @@ void hash_table_delete(hash_table_t *ht)
 	hash_node_t *temp = NULL;
 	hash_node_t *trail = NULL;
 
+	if (!ht)
+		return;
+
 	while (idx < ht->size)
 	{
-		if (ht->array[idx])
+		temp = ht->array[idx];
+		while (temp)
 		{
-			temp = ht->array[idx];
-			while (temp)
-			{
-				trail = temp;
-				temp = temp->next;
-				free(trail->key);
-				free(trail->value);
-				free(trail);
-			}
+			trail = temp;
+			temp = temp->next;
+			free(trail->key);
+			free(trail->value);
+			free(trail);
 		}
 		idx++;
 	}
